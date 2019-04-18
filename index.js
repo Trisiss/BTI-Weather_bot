186ba52ec371205406889171b4f01b3d8108a164
Smirnykh Pavel <trisiss@yandex.ru>
2019-04-18 17:23:12 +0700

    Убрана генерация графика по команде

--- index.js
+++ index.js
@@ -6,22 +6,24 @@ const ChartjsNode = require('chartjs-node');
 
 const token = ''; // токен бота
 
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token, {polling: true});
 
 const URL = 'http://www.bti.secna.ru/web-thermo/index.shtml'; // источник температуры
 
 function doDraw(config, filename) {
-
   // создадим полотно с размером 640x480 пикселей
 
   var chartNode = new ChartjsNode(640, 480);
-
+console.log("doDraw");
+console.log(chartNode.width);
+    console.log(config);
   return chartNode.drawChart(config)
 
       .then(() => {
 
-          // запишем результат в файл 
+          // запишем результат в файл
 
+	console.log("!!!!!!!");
           return chartNode.writeImageToFile('image/png', filename);
 
       });
@@ -40,11 +42,11 @@ function prepareDraw0(v_time){
 
       // здесь могут быть много шагов сбора данных, прежде чем перейти к графику
 
-     .then(()=>{
+      .then(()=>{
 
           // произвольные данные, похожие на те, что хранятся в истории
 
-          testData = [
+         /* testData = [
 
               {"val":3,"ack":1,"ts":1539063874301},
 
@@ -62,7 +64,7 @@ function prepareDraw0(v_time){
 
               {"val":1.23,"ack":1,"ts":1539063944301},
 
-          ];
+          ];*/
 
       })
 
@@ -104,14 +106,9 @@ function prepareDraw0(v_time){
 
             // достанем данные из переменной 'пример' и оставим только значение и время изменения
 
-            // data: v_temp.map((item) =>{
-            //   v_time.map((el) => {
-            //     return {t: new Date(el)}
-            //   })
-            //   return {y: item}
-            // }),
-            data: testData.map((item) => {
-              return {y: item.val, t: new Date(item.ts)}
+            data: v_time.map((item) => {
+		console.log(item.val);
+              return {y: item.temper, t: new Date(item.tm)}
           }),
 
             // заливка графика - нет
@@ -193,7 +190,7 @@ function prepareDraw0(v_time){
         }
 
     };
-
+    
     return chartJsOptions;
 
       });
@@ -216,26 +213,27 @@ function sendGraph0(v_time){
 
           // рисуем картинку по полученным данным и конфигурации
 
+
           return doDraw(result, filename);
 
       })
-
-
       .catch((err)=>{
 
           console.error(err);
 
       });
-
 }
 
+//----------------------------------------------------------------------------------------------------------
 
 
 setInterval(function () { // функция для повторения запроса температуры
+  let v_time = [];
   request(URL, function (err, res, body) {
     if (err) throw err;
     // console.log(body);
     console.log(res.statusCode);
+    let flag = 0;
     // console.log(match);
     let $ = cheerio.load(body);
     let ms_last = ($('html').text());
@@ -243,19 +241,33 @@ setInterval(function () { // функция для повторения запр
     ms_last = ms_last.replace('°C', '');
     ms_last = ms_last.replace(',', '.');
     parseFloat(ms_last);
-    let db = new sqlite3.Database('./db/bot.db', (err) => {
+    var db = new sqlite3.Database('/home/Plex/weather-bot/db/bot.db', (err) => {
       if (err) {
         return console.error(err.message);
       }
       console.log('Connected to the in-memory SQlite database.');
     });
+    return new Promise((resolve, reject) => {
+    console.log("promise");
     let sql = 'SELECT t temp FROM temp_tbl WHERE mark=1 ORDER BY time DESC LIMIT 1';
     db.each(sql, (err, row) => {
       if (err) {
-        return console.error(err.message);
+        // return console.error(err.message);
+        reject(err);
       }
       if (row) {
+    console.log("row");
+    console.log(row.temp);
         if (Math.abs(ms_last - row.temp) >= 5) {
+          flag = 1;
+    console.log("flag = 1");
+          resolve(flag);
+        } else resolve(flag);
+      }
+    });
+  })
+  .then((flag)=>{
+    if (flag == 1) {
           db.run('INSERT INTO temp_tbl(time, t, mark) VALUES(datetime("now", "localtime"), ' + ms_last + ', 1)', function (err) {
             if (err) {
               return console.log(err.message);
@@ -270,7 +282,7 @@ setInterval(function () { // функция для повторения запр
               return console.error(err.message);
             }
             if (row) {
-              bot.onText(row.id, '*Температура в городе изменилась на пять градусов. Сейчас в Бийске *' + ms_last + '°C');
+              bot.sendMessage(row.id, '*Температура в городе изменилась на пять градусов. Сейчас в Бийске *' + ms_last + '°C');
             }
           });
         } else {
@@ -283,12 +295,47 @@ setInterval(function () { // функция для повторения запр
             console.log('Change:' + this.changes);
           });
         }
-      }
-      else console.log('No chat');
-    });
-    db.close();
+    // db.close();
+    //return 0;
+      //});
+      return new Promise((resolve, reject) => {
+    
+        let sql = "SELECT * FROM (SELECT rowid, time tm, t temper FROM temp_tbl ORDER BY time DESC LIMIT 12) ORDER BY rowid";
+
+       //  first row only
+       db.all(sql, (err, rows) => {
+         if (err) {
+          // return console.error(err.message);
+            reject(err);
+         }
+       //if (row)
+	
+         rows.forEach((row) => {
+               v_time.push({"tm":row.tm, "temper":row.temper});
+           console.log(row.tm);
+            console.log(row.temper);
+       });
+       
+        resolve(v_time);
+     // return (v_time);
+       
+       });
+       //resolve(v_time);
+       
+         })
+         .then((v_time) =>{
+           if (v_time.length == 12) {
+           console.log("v_time = 12");
+           sendGraph0(v_time);
+           db.close();
+       }
+       })
+    })
+       .catch((err)=>{
+        console.error(err);
+      });
   });
-}, 120000);
+}, 1800000);
 
 
 //-----------------------------------------------------------------------------------------------------
@@ -301,8 +348,7 @@ bot.onText(/\/start/, msg => { // функция обработки команд
 
   let ms = "Здравствуйте! Здесь вы можете узнать текущую температуру в городе по данным БТИ АлтГТУ. Для того, чтобы узнать данные о погоде, введите /w. Для того, чтобы подписаться\/отписаться на обновление погоды, введите /f. Сейчас в Бийске ";
 
-  // bot.sendMessage(chatId, ms, {"parse_mode": "Markdown"}); // ответ пользователю
-  let db = new sqlite3.Database('./db/bot.db', (err) => {
+  let db = new sqlite3.Database('/home/Plex/weather-bot/db/bot.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
@@ -316,19 +362,17 @@ bot.onText(/\/start/, msg => { // функция обработки команд
     }
     console.log(row + " " + row.temp);
     if (row) {
-      if (row.temp > 0) bot.sendMessage(chatId, ms + "+" + row.temp + "°C Из БД", { "parse_mode": "Markdown" });
-      else bot.sendMessage(chatId, ms + row.temp + "°C Из БД", { "parse_mode": "Markdown" });
+      if (row.temp > 0) bot.sendMessage(chatId, ms + "+" + row.temp + "°C", { "parse_mode": "Markdown" });
+      else bot.sendMessage(chatId, ms + row.temp + "°C", { "parse_mode": "Markdown" });
       flag = 1;
     }
   });
-    /*else {*/ if (flag == 0) {
+    if (flag == 0) {
       console.log('No chat');
       // парсинг температуры, получение начального значения для сравнения с новыми значениями через промежуток времени
       request(URL, function (err, res, body) {
         if (err) throw err;
-        // console.log(body);
         console.log(res.statusCode);
-        // console.log(match);
         let $ = cheerio.load(body); // парсинг температуры
         ms_once = ($('html').text());
         ms_once = ms_once.replace(/\r|\n/g, ''); // удаление символа переноса строки
@@ -337,22 +381,12 @@ bot.onText(/\/start/, msg => { // функция обработки команд
         parseFloat(ms_once);
         console.log(ms_once + " температура загруженная");
         if (ms_once > 0) {
-          bot.sendMessage(chatId, ms + "+" + ms_once + "°C Загрузил", { "parse_mode": "Markdown" });
+          bot.sendMessage(chatId, ms + "+" + ms_once + "°C", { "parse_mode": "Markdown" });
         } else {
-          bot.sendMessage(chatId, ms + ms_once + "°C Загрузил", { "parse_mode": "Markdown" });
+          bot.sendMessage(chatId, ms + ms_once + "°C", { "parse_mode": "Markdown" });
         }
       });
     }
-  // });
-  /*db.each("SELECT * FROM foll_tbl WHERE foll_tbl.chat_id='" + chatId + "'", (err, row) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    if (row) {
-      flagfol = 1;
-    }
-    // else {
-  });*/
       console.log('No chat');
       let sql3 = "INSERT INTO foll_tbl (chat_id, follow) SELECT * FROM (SELECT '" + chatId + "', 0) AS tmp WHERE NOT EXISTS (SELECT chat_id FROM foll_tbl WHERE chat_id = '" + chatId + "') LIMIT 1;";
       db.run(sql3, function (err) {
@@ -363,7 +397,6 @@ bot.onText(/\/start/, msg => { // функция обработки команд
         console.log('A row has been inserted with rowid' + this.lastID);
         console.log('Change:' + this.changes);
       });
-  // });
   db.close();
 });
 
@@ -377,7 +410,7 @@ bot.onText(/\/w/, msg => {  // функция обработки команды 
   const chatId = msg.chat.id;
   console.log(msg);
   let flag = 0;
-  let db = new sqlite3.Database('./db/bot.db', (err) => {
+  let db = new sqlite3.Database('/home/Plex/weather-bot/db/bot.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
@@ -394,12 +427,11 @@ bot.onText(/\/w/, msg => {  // функция обработки команды 
       flag = 1;
     }
   });
-    // else {
+
       if (flag == 0) {
       console.log('No chat');
       request(URL, function (err, res, body) {
         if (err) throw err;
-        // console.log(body);
         console.log(res.statusCode);
         let $ = cheerio.load(body);
         let ms = ($('html').text());
@@ -407,7 +439,6 @@ bot.onText(/\/w/, msg => {  // функция обработки команды 
         ms = ms.replace('°C', '');
         ms = ms.replace(',', '.');
         parseFloat(ms);
-        // console.log(ms);
         if (ms > 0) {
           bot.sendMessage(chatId, "*В Бийске* +" + ms + "°C", { "parse_mode": "Markdown" });
         } else {
@@ -415,7 +446,6 @@ bot.onText(/\/w/, msg => {  // функция обработки команды 
         }
       });
     }
-  // });
   db.close();
 });
 
@@ -425,21 +455,30 @@ bot.onText(/\/w/, msg => {  // функция обработки команды 
 
 bot.onText(/\/f/, msg => {  // функция обработки команды пользователя
   const chatId = msg.chat.id;
+  let fol;
   console.log(msg);
-  let db = new sqlite3.Database('./db/bot.db', (err) => {
+  let db = new sqlite3.Database('/home/Plex/weather-bot/db/bot.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database.');
   });
+  return new Promise((resolve, reject) => {
   let sql = "SELECT follow fol FROM foll_tbl WHERE chat_id='" + chatId + "'";
   db.each(sql, (err, row) => {
     if (err) {
-      return console.error(err.message);
+      reject(err);
     }
     if (row) {
       console.log(row + " " + row.fol);
-      if (row.fol == 0) {
+      fol = row.fol;
+      resolve(fol);
+    }
+  });
+})
+  
+  .then((fol) =>{
+        if (fol == 0) {
         db.run("UPDATE foll_tbl SET follow=1 WHERE chat_id='" + chatId + "'", function (err) {
           if (err) {
             return console.log(err.message);
@@ -451,7 +490,7 @@ bot.onText(/\/f/, msg => {  // функция обработки команды 
         });
         console.log("tsrlskdf");
         bot.sendMessage(chatId, '*Вы подписались на изменения температуры!*', { "parse_mode": "Markdown" });
-      } 
+      }
       else {
         db.run("UPDATE foll_tbl SET follow=0 WHERE chat_id='" + chatId + "'", function (err) {
           if (err) {
@@ -465,44 +504,24 @@ bot.onText(/\/f/, msg => {  // функция обработки команды 
         });
         bot.sendMessage(chatId, '*Вы отписались от изменений температуры!*', { "parse_mode": "Markdown" });
       }
-    } else console.log('No chat');
+    //  else console.log('No chat');
+    db.close();
+    return 0;
   });
-  db.close();
+   
 });
+
 
 //--------------------------------------------------------------------------------------------
 
-bot.onText(/\/p/, msg => {  // функция обработки команды пользователя
+bot.onText(/\/p/, msg => {  // команда для отправки графика температуры
   const chatId = msg.chat.id;
   console.log(msg);
-  let v_time = [];
-  let v_temp = [];
-  let db = new sqlite3.Database('./db/bot.db', (err) => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-  });
- let sql = "SELECT * FROM (SELECT rowid, time tm, t temper FROM temp_tbl ORDER BY time DESC LIMIT 5) ORDER BY rowid";
 
-//  first row only
-db.each(sql, (err, row) => {
-  if (err) {
-    return console.error(err.message);
-  }
-    if (row) {
-        v_time.push({"tm":row.tm, "temper":row.temper});
-    // v_time.push(row.tm);
-    console.log(row.tm);
-    //  v_temp.push(row.temper);
-     console.log(row.temper); 
-    }
-    console.log(typeof(v_temp));
-  
+  bot.sendPhoto(chatId, "graph0.png", {
+  caption: "График погоды за 6 часов"
 });
-db.close();
-  console.log("TRUE");
-  sendGraph0(v_temp, v_time);
+console.log("Send graph");
 });
 
 //----------------------------------------------------------------------------------------------------------
@@ -511,8 +530,8 @@ db.close();
 bot.on('message', msg => {
   const user = msg.chat.username;
   let mes = msg.text;
-  mes = mes.replace('/', '');
-  let db = new sqlite3.Database('./db/bot.db', (err) => {
+ // mes = mes.replace('/', '');
+  let db = new sqlite3.Database('/home/Plex/weather-bot/db/bot.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
@@ -529,9 +548,8 @@ bot.on('message', msg => {
 
   });
  db.close();
-  // send a message to the chat acknowledging receipt of their message
-  // bot.sendMessage(chatId, 'Неизвестная команда.');
 });
+
 
 
 
